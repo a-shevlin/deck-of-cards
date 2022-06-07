0698a63079280b80b19ba977c8cd71062418c323
Alex Shevlin <alexshevlin1@gmail.com>
2022-06-06 17:09:06 -0700

    fix deck to organize on list.

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,16 @@
 $(document).ready(function() {
-  $(".cards").submit(function(event) {
+  $("form#cards").submit(function(event) {
     event.preventDefault();
     const numberArray = ["Ace","2","3","4","5","6","7","8","9","10","Jack","Queen","King"];
     const suitArray = ["Hearts","Spades","Diamonds","Clubs"];
+    console.log(suitArray);
     let deck = [];
     suitArray.forEach(function(suit) {
       numberArray.forEach(function(number){
-        deck.push(number + "of" + suit);
+        deck.push(number + " of " + suit);
+        $("#output").append("<li>" + deck.toString().split(",").pop() + "</li>");
       });
     }); 
-    deck.forEach(function(card) {
-      $("#output").append("<li>" + deck.toString().split(",") +"</li>");
-    });
+
   });
 });
